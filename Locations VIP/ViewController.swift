529713c5cde45e45edc500d2344add8d4fa54012
Bennet van der Linden <Bennet.vanderLinden@wearetriple.com>
2025-07-08 20:58:23 +0200

    Initial Commit

---
+++ Locations VIP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Locations VIP
+//
+//  Created by Bennet van der Linden on 08/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
